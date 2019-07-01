f069d6ef8c5262c28cd58172a14a778127b2dec0
Jeremy Gerstle <jgerstle@realoptimal.com>
2019-06-30 20:59:52 -0700

    add a unit test for factor

--- test_app.py
+++ test_app.py
@@ -7,8 +7,15 @@ from wq_app.framework.models import *
 
 class TestSampleModel(unittest.TestCase):
     def setUp(self):
-        db.session.close()
-        db.drop_all()
-        db.create_all()
-        # add in some illustrative data
-        
+        # db.session.close()
+        # db.drop_all()
+        # db.create_all()
+        pass
+    
+    def test_get_sample_factor(self):
+        sample2 = Sample.query.get(2)
+        factor_val = sample2.factor(1)
+        self.assertEqual(factor_val, 0.024007)
+
+    def tearDown(self):
+        pass
